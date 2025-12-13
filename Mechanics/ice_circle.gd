7929d94934866bd924942f85bf0a818010b42e38
gnorris55 <graemenorris99@gmail.com>
2025-12-13 14:21:00 -0800

    FIXED EVERYTHING

--- Mechanics/ice_circle.gd
+++ Mechanics/ice_circle.gd
@@ -7,11 +7,13 @@ signal ice_placed(position: Vector3, rotation: Vector3)
 @onready var ice_placement_indictor: MeshInstance2D = $icePlacementIndictor
 @onready var ice_rectangle: Node2D = $IceRectangle
 
+
 @export var ice_placement_radius = 2
 
 var mouse_direction: Vector2
 var ice_placement_direction: Vector2
 var spawning_ice: bool = false
+
 
 
 func _physics_process(delta: float) -> void:
--- Mechanics/ice_rectangle.gd
+++ Mechanics/ice_rectangle.gd
@@ -2,9 +2,10 @@ extends Node2D
 class_name IceRectangle
 
 signal melted(node_id)
-
+signal ice_hit(projectile_position, projectile_radius)
 
 @export var melting_time: float = 2.0
+@onready var spawn_ice_effect: CPUParticles2D = $spawnIceEffect
 
 @onready var melt_timer: Timer = $meltTimer
 
@@ -12,6 +13,7 @@ signal melted(node_id)
 func _ready() -> void:
 	melt_timer.wait_time = melting_time
 	melt_timer.start()
+	spawn_ice_effect.emitting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,6 +21,17 @@ func _process(delta: float) -> void:
 	pass
 
 
+
+
 func _on_melt_timer_timeout() -> void:
+	remove_self()
+	
+func remove_self():
 	melted.emit(self)
 	queue_free()
+	
+
+
+func _on_projectile_detector_area_entered(area: Area2D) -> void:
+	print()
+	ice_hit.emit(area.global_position, 3)
