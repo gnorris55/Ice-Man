5d72d839492e693982c655db9e3f943cd37db42b
gnorris55 <graemenorris99@gmail.com>
2025-12-13 16:16:13 -0800

    added ice deposit

--- Mechanics/ice_circle.gd
+++ Mechanics/ice_circle.gd
@@ -3,9 +3,10 @@ extends Node2D
 signal ice_placed(position: Vector3, rotation: Vector3)
 
 
-
 @onready var ice_placement_indictor: MeshInstance2D = $icePlacementIndictor
 @onready var ice_rectangle: Node2D = $IceRectangle
+
+@onready var ice_bar: ProgressBar = $CanvasLayer/MarginContainer/IceBar
 
 
 @export var ice_placement_radius = 2
@@ -14,6 +15,15 @@ var mouse_direction: Vector2
 var ice_placement_direction: Vector2
 var spawning_ice: bool = false
 
+@export var full_health = 50.0
+@export var placement_cost = 0.5
+var current_health = 50
+
+
+func _ready() -> void:
+	current_health = full_health
+	ice_bar.value = full_health
+	ice_bar.max_value = full_health
 
 
 func _physics_process(delta: float) -> void:
@@ -29,7 +39,8 @@ func _physics_process(delta: float) -> void:
 	
 func spawn_ice_rectangle():
 	ice_placed.emit((mouse_direction*ice_placement_radius) + global_position, ice_placement_direction.angle())
-	
+	current_health -= placement_cost
+	ice_bar.value = current_health
 	
 
 func get_mouse_direction():
---
+++ gameObject/ice_deposit.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player collision"):
+		
+		var ice_circle = area.get_node("./../iceCircle")
+		ice_circle.current_health = ice_circle.full_health
+		ice_circle.ice_bar.value = ice_circle.full_health
+		
+		queue_free()
+	
